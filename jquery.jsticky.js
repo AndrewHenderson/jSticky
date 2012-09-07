fea795191c8b8adde3d4ef51dc55ea459d6b7d34
AndrewHenderson <andrew.m.henderson@gmail.com>
2012-09-07 01:05:30 -0700

    first commit

---
+++ jquery.jsticky.js
@@ -0,0 +1,124 @@
+/* jSticky Plugin
+ * =============
+ * Author: Andrew Henderson (@AndrewHenderson)
+ * Date: 9/7/2012
+ * Website: http://github.com/andrewhenderson/jsticky/
+ * Description: A jQuery plugin that allows any DOM element to remain in
+ * view while scrolling the page.
+ */
+
+(function ( $ ) {
+
+	$.fn.sticky = function ( options ) {
+
+		var defaults = {
+				topSpacing: 0, // No spacing by default
+				zIndex: '', // No default z-index
+				stopper: '.stopper' // Default stopper class, also accepts number value
+			},
+			settings = $.extend({}, defaults, options); // Accepts custom stopper id or class
+
+		function checkIndex() {
+
+			// Checks if custom z-index was defined
+			if( typeof settings.zIndex == 'number' ) {
+
+				return true;
+
+			} else {
+
+				return false;
+
+			}
+
+		}
+		var hasIndex = checkIndex(); // True or false
+
+		function checkStopper() {
+
+			// Checks if a stopper exists in the DOM or number defined
+			if ( 0 < $(settings.stopper).length || typeof settings.stopper === 'number' ) {
+
+				return true;
+
+			} else {
+
+				return false;
+
+			}
+
+		}
+		var hasStopper = checkStopper(); // True or false
+
+		return this.each(function () {
+
+			var $this          = $(this),
+				thisHeight     = $this.outerHeight(),
+				topSpacing     = settings.topSpacing,
+				zIndex         = settings.zIndex,
+				pushPoint      = $this.offset().top - topSpacing, // Point at which the sticky element starts pushing
+				placeholder    = $this.clone().css("visibility","hidden").addClass("sticky-placeholder"), // Cache a clone sticky element
+				stopper        = settings.stopper,
+				$window        = $(window);
+
+			function stickyScroll () {
+
+				var windowTop  = $(window).scrollTop(); // Check window's scroll position
+				
+				if ( hasStopper && typeof stopper === 'string' ) {
+
+					var stopperTop = $(stopper).offset().top,
+						stopPoint  = (stopperTop - thisHeight) - topSpacing;
+				
+				} else if ( hasStopper && typeof stopper === 'number' ) {
+
+					var stopPoint = stopper;
+
+				}
+
+				if ( pushPoint < windowTop ){
+
+					// Create a placeholder for sticky element to occupy vertical real estate
+					$this.after(placeholder)
+						 .css({
+							position: 'fixed',
+							top: topSpacing
+					});
+					
+					if ( hasIndex ) {
+						
+						$this.css({ zIndex: zIndex });
+
+					}
+
+					if ( hasStopper ) {
+						
+						if ( stopPoint < windowTop ) {
+
+							var diff = (stopPoint - windowTop) + topSpacing;
+
+							$this.css({ top: diff });
+
+						}
+
+					}
+
+				} else {
+
+					$this.css({
+						position: 'static',
+						top: ''
+					});
+
+					var clone = $this.siblings(".sticky-placeholder");
+
+					$(clone).remove();
+
+				}
+
+			};
+
+			$window.bind("scroll", stickyScroll);
+		});
+	};
+})(jQuery);
