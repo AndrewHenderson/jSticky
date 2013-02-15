c40587685bec6f9a7540fec130d4b4f1e924e056
Andrew Henderson <andrew.m.henderson@gmail.com>
2013-02-14 23:03:56 -0800

    Using simple div element for placeholder rather than more costly clone.

--- jquery.jsticky.js
+++ jquery.jsticky.js
@@ -2,122 +2,97 @@
  * =============
  * Author: Andrew Henderson (@AndrewHenderson)
  * Date: 9/7/2012
- * Update: 11/4/2012
+ * Update: 02/14/2013
  * Website: http://github.com/andrewhenderson/jsticky/
- * Description: A jQuery plugin that keeps select DOM element(s)
- * in view while scrolling the page.
+ * Description: A jQuery plugin that keeps select DOM
+ * element(s) in view while scrolling the page.
  */
 
-;(function ( $ ) {
+;(function($) {
 
-	$.fn.sticky = function ( options ) {
+  $.fn.sticky = function(options) {
 
-		var defaults = {
-				topSpacing: 0, // No spacing by default
-				zIndex: '', // No default z-index
-				stopper: '.sticky-stopper' // Default stopper class, also accepts number value
-			},
-			settings = $.extend({}, defaults, options); // Accepts custom stopper id or class
+    var defaults = {
+        topSpacing: 0, // No spacing by default
+        zIndex: '', // No default z-index
+        stopper: '.sticky-stopper' // Default stopper class, also accepts number value
+      },
+      settings = $.extend({}, defaults, options); // Accepts custom stopper id or class
 
-		// Checks if custom z-index was defined
-		function checkIndex() {
+    // Checks if custom z-index was defined
+    function checkIndex() {
 
-			if ( typeof settings.zIndex == 'number' ) {
+      if (typeof settings.zIndex == 'number') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    var hasIndex = checkIndex(); // True or false
 
-				return true;
+    // Checks if a stopper exists in the DOM or number defined
+    function checkStopper() {
 
-			} else {
+      if ( 0 < $(settings.stopper).length || typeof settings.stopper === 'number' ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    var hasStopper = checkStopper(); // True or false
 
-				return false;
+    return this.each(function() {
 
-			}
+      var $this          = $(this),
+          thisHeight     = $this.outerHeight(),
+          thisWidth      = $this.outerWidth(),
+          topSpacing     = settings.topSpacing,
+          zIndex         = settings.zIndex,
+          pushPoint      = $this.offset().top - topSpacing, // Point at which the sticky element starts pushing
+          placeholder    = $('<div></div>').width(thisWidth).height(thisHeight).addClass('sticky-placeholder'), // Cache a clone sticky element
+          stopper        = settings.stopper,
+          $window        = $(window);
 
-		}
-		var hasIndex = checkIndex(); // True or false
+      function stickyScroll() {
 
-		// Checks if a stopper exists in the DOM or number defined
-		function checkStopper() {
+        var windowTop  = $window.scrollTop(); // Check window's scroll position
+        
+        if ( hasStopper && typeof stopper === 'string' ) {
+          var stopperTop = $(stopper).offset().top,
+              stopPoint  = (stopperTop - thisHeight) - topSpacing;
+        } else if (hasStopper && typeof stopper === 'number') {
+          var stopPoint = stopper;
+        }
 
-			if ( 0 < $(settings.stopper).length || typeof settings.stopper === 'number' ) {
+        if (pushPoint < windowTop) {
+          // Create a placeholder for sticky element to occupy vertical real estate
+          $this.after(placeholder).css({
+            position: 'fixed',
+            top: topSpacing
+          });
+          
+          if (hasIndex) {
+            $this.css({ zIndex: zIndex });
+          }
 
-				return true;
+          if (hasStopper) {
+            if (stopPoint < windowTop) {
+              var diff = (stopPoint - windowTop) + topSpacing;
+              $this.css({ top: diff });
+            }
+          }
+        } else {
+          $this.css({
+            position: 'static',
+            top: null,
+            left: null
+          });
 
-			} else {
+          placeholder.remove();
+        }
+      };
 
-				return false;
-
-			}
-
-		}
-		var hasStopper = checkStopper(); // True or false
-
-		return this.each(function () {
-
-			var $this          = $(this),
-				thisHeight     = $this.outerHeight(),
-				topSpacing     = settings.topSpacing,
-				zIndex         = settings.zIndex,
-				pushPoint      = $this.offset().top - topSpacing, // Point at which the sticky element starts pushing
-				placeholder    = $this.clone().css("visibility","hidden").addClass("sticky-placeholder"), // Cache a clone sticky element
-				stopper        = settings.stopper,
-				$window        = $(window);
-
-			function stickyScroll () {
-
-				var windowTop  = $window.scrollTop(); // Check window's scroll position
-				
-				if ( hasStopper && typeof stopper === 'string' ) {
-
-					var stopperTop = $(stopper).offset().top,
-						stopPoint  = ( stopperTop - thisHeight ) - topSpacing;
-				
-				} else if ( hasStopper && typeof stopper === 'number' ) {
-
-					var stopPoint = stopper;
-
-				}
-
-				if ( pushPoint < windowTop ) {
-
-					// Create a placeholder for sticky element to occupy vertical real estate
-					$this.after(placeholder)
-						 .css({
-							position: 'fixed',
-							top: topSpacing
-					});
-					
-					if ( hasIndex ) {
-						
-						$this.css({ zIndex: zIndex });
-
-					}
-
-					if ( hasStopper ) {
-						
-						if ( stopPoint < windowTop ) {
-
-							var diff = ( stopPoint - windowTop ) + topSpacing;
-
-							$this.css({ top: diff });
-
-						}
-
-					}
-
-				} else {
-
-					$this.css({
-						position: 'static',
-						top: ''
-					});
-
-					placeholder.remove();
-
-				}
-
-			};
-
-			$window.bind("scroll", stickyScroll);
-		});
-	};
-})( jQuery );
+      $window.bind("scroll", stickyScroll);
+    });
+  };
+})(jQuery);
